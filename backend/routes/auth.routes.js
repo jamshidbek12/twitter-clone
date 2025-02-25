8755867bc8cadd1e7b1ffa520d6025b7ff7ad44c
Jamshidbek Zaripov <jdzaripov@gmail.com>
2025-02-25 20:36:02 +0500

    1-auth controllers added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import {
+  getMe,
+  login,
+  logout,
+  signup,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = Router();
 
-router.get("/signup", signup);
+router.get("/me", protectRoute, getMe);
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
-router.get("/login", login);
-
-router.get("/logout", logout);
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,17 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectDB from "./db/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
