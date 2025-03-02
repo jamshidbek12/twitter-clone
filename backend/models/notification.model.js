469d6e0c384f2635f438339ea6c9d668db0e28a1
Jamshidbek Zaripov <jdzaripov@gmail.com>
2025-03-02 11:53:35 +0500

    1-user routes and controllers added

---
+++ backend/models/notification.model.js
@@ -0,0 +1,32 @@
+import { Schema, model } from "mongoose";
+
+const notificationSchema = new Schema(
+  {
+    from: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    to: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["follow", "like"],
+      required: true,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Notification = model("Notification", notificationSchema);
+
+export default Notification;
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.routes.js";
+import usersRoutes from "./routes/user.routes.js";
+
 import connectDB from "./db/db.js";
-import cookieParser from "cookie-parser";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,6 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", usersRoutes);
 
 app.listen(PORT, () => {
   connectDB();
