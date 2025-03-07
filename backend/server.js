fc207a89d4cd9b887ba0486940b0b8a61654e98d
Jamshidbek Zaripov <jdzaripov@gmail.com>
2025-03-07 12:47:43 +0500

    1 - update route files and paths

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 
-import authRoutes from "./routes/auth.routes.js";
-import usersRoutes from "./routes/user.routes.js";
-import postRoutes from "./routes/post.routes.js";
+import authRoutes from "./routes/auth.route.js";
+import usersRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
 
 import connectDB from "./db/db.js";
 
