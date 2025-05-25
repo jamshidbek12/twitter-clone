c2be1e26144d2780abff4688291b107913f66a9e
Jamshidbek Zaripov <jdzaripov@gmail.com>
2025-05-25 10:28:17 +0500

    3- Update user profile  in edit  profile modal

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -141,25 +141,25 @@ export const updateUser = async (req, res) => {
       user.password = await bcrypt.hash(newPassword, salt);
     }
 
-    if (profileImg) {
-      if (user.profileImg) {
-        await cloudinary.uploader.destroy(
-          user.profileImg.split("/").pop().split(".")[0]
-        );
-      }
-      const uploadedResponse = await cloudinary.uploader.upload(profileImg);
-      profileImg = uploadedResponse.secure_url;
-    }
+    // if (profileImg) {
+    //   if (user.profileImg) {
+    //     await cloudinary.uploader.destroy(
+    //       user.profileImg.split("/").pop().split(".")[0]
+    //     );
+    //   }
+    //   const uploadedResponse = await cloudinary.uploader.upload(profileImg);
+    //   profileImg = uploadedResponse.secure_url;
+    // }
 
-    if (coverImg) {
-      if (user.coverImg) {
-        await cloudinary.uploader.destroy(
-          user.coverImg.split("/").pop().split(".")[0]
-        );
-      }
-      const uploadedResponse = await cloudinary.uploader.upload(coverImg);
-      coverImg = uploadedResponse.secure_url;
-    }
+    // if (coverImg) {
+    //   if (user.coverImg) {
+    //     await cloudinary.uploader.destroy(
+    //       user.coverImg.split("/").pop().split(".")[0]
+    //     );
+    //   }
+    //   const uploadedResponse = await cloudinary.uploader.upload(coverImg);
+    //   coverImg = uploadedResponse.secure_url;
+    // }
 
     user.fullName = fullName || user.fullName;
     user.email = email || user.email;
--- frontend/src/pages/profile/EditProfileModal.jsx
+++ frontend/src/pages/profile/EditProfileModal.jsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
-const EditProfileModal = () => {
+const EditProfileModal = ({ authUser }) => {
+  const queryClient = useQueryClient();
   const [formData, setFormData] = useState({
     fullName: "",
     username: "",
@@ -11,14 +14,59 @@ const EditProfileModal = () => {
     currentPassword: "",
   });
 
+  const { mutate: updateProfile, isPending: isUpdatingProfile } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch("/api/users/update", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.message || "Failed to update profile");
+        }
+        return data;
+      } catch (error) {
+        throw new Error("Failed to update profile", error);
+      }
+    },
+    onSuccess: () => {
+      toast.success("Profile updated successfully");
+      Promise.all([
+        queryClient.invalidateQueries(["userProfile"]),
+        queryClient.invalidateQueries(["authUser"]),
+      ]);
+    },
+    onError: (error) => {
+      toast.error(error.message || "Failed to update profile");
+    },
+  });
+
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  useEffect(() => {
+    if (authUser) {
+      setFormData({
+        fullName: authUser.fullName || "",
+        username: authUser.username || "",
+        email: authUser.email || "",
+        bio: authUser.bio || "",
+        link: authUser.link || "",
+        newPassword: "",
+        currentPassword: "",
+      });
+    }
+  }, [authUser]);
+
   return (
     <>
       <button
-        className="btn btn-outline rounded-full btn-sm"
+        className="btn btn-outline btn-primary rounded-full btn-sm"
         onClick={() =>
           document.getElementById("edit_profile_modal").showModal()
         }
@@ -32,7 +80,7 @@ const EditProfileModal = () => {
             className="flex flex-col gap-4"
             onSubmit={(e) => {
               e.preventDefault();
-              alert("Profile updated successfully");
+              updateProfile();
             }}
           >
             <div className="flex flex-wrap gap-2">
@@ -97,7 +145,7 @@ const EditProfileModal = () => {
               onChange={handleInputChange}
             />
             <button className="btn btn-primary rounded-full btn-sm text-white">
-              Update
+              {isUpdatingProfile ? "Updating..." : "Update"}
             </button>
           </form>
         </div>
