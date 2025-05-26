dd7c0e25c1b9f3554da187eec19515e311c28320
Jamshidbek Zaripov <jdzaripov@gmail.com>
2025-05-26 20:47:22 +0500

    4- prepared our app for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import usersRoutes from "./routes/user.route.js";
@@ -20,6 +21,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" }));
 // Limit shouldn't be too large to prevent DOS attacks
@@ -31,6 +33,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", usersRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
