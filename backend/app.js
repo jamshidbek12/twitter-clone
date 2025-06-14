4a558d283e026911d5a8e932435128b1526e9e3d
Jamshidbek <jdzaripov@gmail.com>
2025-06-14 16:27:23 +0500

    Update and rename server.js to app.js
    for test

--- backend/app.js
+++ backend/app.js
@@ -32,6 +32,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", usersRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/search", searchRoutes);
 app.use("/api/notifications", notificationRoutes);
 
 if (process.env.NODE_ENV === "production") {
